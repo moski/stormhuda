de7e9fddcad7778435370273de135e3554d35346
Moski Doski <abushaikh@gmail.com>
2015-01-09 15:23:26 +0200

    pushing instagram to global list

--- fetchers/storage/insta.rb
+++ fetchers/storage/insta.rb
@@ -3,11 +3,11 @@ module Storage
     def save!(conn)
       # Store the tweet in the tweets collection and Global
       conn.lpush self.redis_keys['instagram']['key'], self.to_json
-      #conn.lpush self.redis_keys['global']['key'], self.to_json
+      conn.lpush self.redis_keys['global']['key'], self.to_json
       
       # Increments the counter for the tweets and global
       tweet_counter  = conn.incrby self.redis_keys['instagram']['counter'], 1
-      #global_counter = conn.incrby self.redis_keys['global']['counter'], 1
+      global_counter = conn.incrby self.redis_keys['global']['counter'], 1
     
       self.publish!(conn)
     end
@@ -15,11 +15,11 @@ module Storage
     def publish!(conn)
       # Pubsub for counters
       conn.publish(self.redis_keys['instagram']['pubsub']['counter'], tweet_counter)
-      #conn.publish(self.redis_keys['global']['pubsub']['counter'], global_couter)      
+      conn.publish(self.redis_keys['global']['pubsub']['counter'], global_couter)      
       
       # Pubsub for the tweets
       conn.publish(self.redis_keys['instagram']['pubsub']['list'], self.to_json)
-      #conn.publish(self.redis_keys['global']['pubsub']['list'], self.to_json)
+      conn.publish(self.redis_keys['global']['pubsub']['list'], self.to_json)
    end 
   end
 end
