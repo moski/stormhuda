34000318378504da882aeac16f8c803c42adbb80
Moski Doski <abushaikh@gmail.com>
2015-01-09 22:33:47 +0200

    Fixing the pub sub counter

--- fetchers/storage/insta.rb
+++ fetchers/storage/insta.rb
@@ -9,17 +9,14 @@ module Storage
       tweet_counter  = conn.incrby self.redis_keys['instagram']['counter'], 1
       global_counter = conn.incrby self.redis_keys['global']['counter'], 1
     
-      self.publish!(conn)
-    end
-    
-    def publish!(conn)
-      # Pubsub for counters
       conn.publish(self.redis_keys['instagram']['pubsub']['counter'], tweet_counter)
       conn.publish(self.redis_keys['global']['pubsub']['counter'], global_couter)      
       
       # Pubsub for the tweets
       conn.publish(self.redis_keys['instagram']['pubsub']['list'], self.to_json)
       conn.publish(self.redis_keys['global']['pubsub']['list'], self.to_json)
-   end 
+    end
+    
+    
   end
 end
